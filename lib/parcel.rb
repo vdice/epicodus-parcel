2b4aa2bd7bddd081c63084f8986784fe68c3b79b
Clayton Easterbrooks & Vaughn Dice <student@epicodus.com>
2015-08-10 12:04:17 -0700

    calculate cost to ship

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,11 +1,29 @@
 class Parcel
-  define_method(:initialize) do |width, height, depth|
+  STANDARD_DISTANCE = 500
+  STANDARD_VOLUME = 72
+  STANDARD_WEIGHT = 1
+  STANDARD_COST = 5
+
+  define_method(:initialize) do |width, height, depth, weight, service, distance|
     @width = width
     @height = height
     @depth = depth
+    @weight = weight
+    @service = service
+    @distance = distance
   end
 
   define_method(:volume) do
     @width.*(@height).*(@depth)
+  end
+
+  define_method(:cost_to_ship) do
+    cost = STANDARD_COST
+
+    cost += 10 if volume().>(STANDARD_VOLUME)
+    cost += @weight.*(0.5) if @weight.>(STANDARD_WEIGHT)
+    cost += 5 if @service.downcase().==('expedited')
+    cost += 15 if @service.downcase().==('overnight')
+    cost += ((@distance.-(STANDARD_DISTANCE)).*(0.01)) if @distance.>(STANDARD_DISTANCE)
   end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -4,8 +4,19 @@ require('parcel')
 describe(Parcel) do
   describe("#volume") do
     it("Returns volume of parcel") do
-      test_parcel = Parcel.new(3, 3, 4)
+      test_parcel = Parcel.new(3, 3, 4, 0, 'Standard', 0)
       expect(test_parcel.volume()).to(eq(36))
+    end
+
+    describe("#cost_to_ship") do
+      it("Returns the cost to ship a standard package") do
+        test_parcel = Parcel.new(3, 3, 4, 5, 'Standard', 3000)
+        expect(test_parcel.cost_to_ship()).to(eq(32.50))
+      end
+      it("Returns the cost to ship an expedited package") do
+        test_parcel = Parcel.new(3, 3, 4, 5, 'Expedited', 3000)
+        expect(test_parcel.cost_to_ship()).to(eq(37.50))
+      end
     end
   end
 end
